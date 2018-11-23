09fdd27ed78e82fe4426533a6ff067a15a429ad3
mikreo <e.fabiano@icloud.com>
2018-11-23 15:18:39 +0000

    added an image to embed for testing

--- commands/embed.js
+++ commands/embed.js
@@ -6,7 +6,7 @@ module.exports = {
     const Embeded = new Discord.RichEmbed()
     .setColor('#00ffcc')
     .setTitle('Here is your meme!')
-
+    .setImage('https://cdn.discordapp.com/attachments/463478570853859330/507795794225528832/image9.jpg')
     message.channel.send(Embeded)
 
 
