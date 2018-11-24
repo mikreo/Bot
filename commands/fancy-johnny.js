210d3f02d8b1583ce67dc7978ae254aaf6a1cc1e
mikreo <e.fabiano@icloud.com>
2018-11-24 18:14:44 +0000

    Added a secret fancy johnny command.

---
+++ commands/fancy-johnny.js
@@ -0,0 +1,7 @@
+module.exports={
+  name:"fajohnny",
+  description:"Fancy johnny.",
+  execute(message,args){
+    return message.channel.send('Jonathon Jonathon?\nYes father?\nConsuming glucose?\nNo father.\nDeciving me?\nNo father.\nOpen your mouth.\nHohoho.')
+  }
+}
