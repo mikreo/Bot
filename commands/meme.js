6630486ac5240ca3f33a6aec6ef18aaa4ea352d0
mikreo <e.fabiano@icloud.com>
2018-12-07 20:13:45 +0000

    n

--- commands/meme.js
+++ commands/meme.js
@@ -7,54 +7,54 @@ module.exports = {
   return Math.floor(Math.random() * (high - low + 1) + low)}
 
   var randNum = randomInt(1,9)
-  const Embeded = new Discord.RichEmbed()
+  const Embed = new Discord.RichEmbed()
   .setColor('#00ffcc')
   .setTitle('Here is your meme!')
 
 
   switch (randNum) {
     case 1:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507795794225528832/image9.jpg')
-      return message.channel.send(Embeded)
+      Embed.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507795794225528832/image9.jpg')
+      return message.channel.send(Embed)
 
     case 2:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507795798411575311/image3.jpg')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507795798411575311/image3.jpg')
+      return message.channel.send(Embed);
       break;
 
     case 3:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507900869036212244/image0.jpg')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/463478570853859330/507900869036212244/image0.jpg')
+      return message.channel.send(Embed);
       break;
 
     case 4:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/463478570853859330/508301294012989462/image0.jpg')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/463478570853859330/508301294012989462/image0.jpg')
+      return message.channel.send(Embed);
       break;
 
     case 5:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/463478570853859330/506647909823938569/image0.jpg')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/463478570853859330/506647909823938569/image0.jpg')
+      return message.channel.send(Embed);
       break;
 
     case 6:
-      Embeded.setImage('https://media.discordapp.net/attachments/463478570853859330/506601243070693396/image0.jpg')
-      return message.channel.send(Embebed);
+      Embed.setImage('https://media.discordapp.net/attachments/463478570853859330/506601243070693396/image0.jpg')
+      return message.channel.send(Embed);
       break;
 
     case 7:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741634543452197/Z.png')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741634543452197/Z.png')
+      return message.channel.send(Embed);
       break;
 
     case 8:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741735806271498/images.png')
-      return message.channel.send(Embeded);
+      Embed.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741735806271498/images.png')
+      return message.channel.send(Embed);
       break;
 
     case 9:
-      Embeded.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741953608220702/images.png')
-      return message.channel.send(Embeded)
+      Embed.setImage('https://cdn.discordapp.com/attachments/305794496140935168/512741953608220702/images.png')
+      return message.channel.send(Embed)
 
     default:
       return message.channel.send('Somthing went wrong!')
