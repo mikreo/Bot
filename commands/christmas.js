9a814591a25a4d0e7f96be4a49c5c7d75a5d0419
mikreo <e.fabiano@icloud.com>
2018-12-22 11:03:49 +0000

    added a test christmas command that outputs the month for now

---
+++ commands/christmas.js
@@ -0,0 +1,9 @@
+module.exports = {
+  name: 'christmas',
+  description:'Gives christmasy things.',
+  args: true
+  execute(message,args)
+    let D = Date()
+    let Month = D.getMonth()
+    return message.channel.send(Month);
+}
