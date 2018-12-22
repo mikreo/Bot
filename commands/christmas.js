9e8fa722e357624a133142725696ed5cbc03ca63
mikreo <e.fabiano@icloud.com>
2018-12-22 11:30:39 +0000

    added 1 joke

--- commands/christmas.js
+++ commands/christmas.js
@@ -1,8 +1,25 @@
 module.exports = {
   name: 'christmas',
   description:'Gives christmasy things.',
+  args: true,
   execute(message,args){
     let D = new Date()
     let Month = D.getMonth()
-    return message.channel.send(Month);}
+    if (Month == 11) {
+      if (args[0]=='joke') {
+        switch (args[1]) {
+          case '1':
+            return message.channel.send('What do you call an old snowman?\nWater!')
+            break;
+          default:
+
+        }
+      }
+
+
+
+    } else {
+      return message.channel.send('It\'s not even december.')
+    }
+  }
 }
