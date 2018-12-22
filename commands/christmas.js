13b3d64d1e2761a39e6be345a3fe36004b2e2f3b
mikreo <e.fabiano@icloud.com>
2018-12-22 11:09:36 +0000

    moar n

--- commands/christmas.js
+++ commands/christmas.js
@@ -2,7 +2,7 @@ module.exports = {
   name: 'christmas',
   description:'Gives christmasy things.',
   execute(message,args){
-    let D = Date()
+    let D = new Date()
     let Month = D.getMonth()
     return message.channel.send(Month);}
 }
